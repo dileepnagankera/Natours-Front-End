ebaa6d1ed14967939fe2d5dc60b185acd6c0d941
dileepnagankera <dileepnagankera@gmail.com>
2024-08-28 15:53:23 +0530

    Initialize Priority

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="flex flex-col md:flex-row justify-between bg-gray-500 text-center text-white p-4 m-4 rounded-lg">
+        <h1 className="text-2xl text-red-200">Logo</h1>
+        <nav className="mt-4 md:mt-0">
+          <ul className="flex flex-col md:flex-row gap-2 md:gap-5">
+            <li>HOME</li>
+            <li>About</li>
+            <li>Contact</li>
+            <li>Services</li>
+            <li>Footer</li>
+            <li>Car</li>
+          </ul>
+        </nav>
+      </div>
     </div>
   );
 }
